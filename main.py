0a20bd03511b0d1efca457e22769d5325e966b7f
32ba <admin@32ba.net>
2019-12-18 11:34:04 +0900

    権限不足の際、再連携を促すメッセージの追加

--- main.py
+++ main.py
@@ -15,27 +15,30 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return
+            return
+    try:
+        #--character--
+        if message.content.startswith('!character make'):
+            await message.channel.send(f'{message.author.mention} ' +str(character.make()))
+            await message.guild.create_text_channel("channelName")
+        if message.content.startswith('!character load'):
+            index = message.content.replace("!character load ","")
+            await message.channel.send(f'{message.author.mention} ' +str(character.load(index)))
 
-    #--character--
-    if message.content.startswith('!character make'):
-        await message.channel.send(f'{message.author.mention} ' +str(character.make()))
-    if message.content.startswith('!character load'):
-        index = message.content.replace("!character load ","")
-        await message.channel.send(f'{message.author.mention} ' +str(character.load(index)))
+        #--help--
+        if message.content.startswith('!help'):
+            return
 
-    #--help--
-    if message.content.startswith('!help'):
-        return
-
-    #--dice--
-    if message.channel.name in Channels:
-        try:
-            n,d = map(int,message.content.split('d'))
-        except ValueError:
-            await message.channel.send(f"{message.author.mention}正しいフォーマットで入力してください.")
-        else:
-            await message.channel.send(f'{message.author.mention} result : '+ str(dice.roll(n,d)))
+        #--dice--
+        if message.channel.name in Channels:
+            try:
+                n,d = map(int,message.content.split('d'))
+            except ValueError:
+                return
+            else:
+                await message.channel.send(f'{message.author.mention} result : '+ str(dice.roll(n,d)))
+    except discord.errors.Forbidden:
+        await message.channel.send("権限不足です。\n新しく追加された機能を使う際、Botの再連携が必要な場合があります。\n詳しくは下記のURLを参照してください。\nhttps://github.com/32ba/TRPGTool/README.md")
         
 
 client.run(os.environ["SECRET_TOKEN"])
